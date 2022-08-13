a5a7cc0624d6a529773704ccb0e92f8cf6d67eae
Vithu05 <71621913+Vithursh@users.noreply.github.com>
2022-08-12 22:34:44 -0700

    Add files via upload

---
+++ Snake Game/src/Main.java
@@ -0,0 +1,12 @@
+ import java.io.File;
+import java.io.IOException;
+import javax.sound.sampled.*;
+
+public class Main {
+
+	public static void main(String[] args) throws UnsupportedAudioFileException, IOException, LineUnavailableException{
+		// TODO Auto-generated method stub
+
+		new SnakeCanvas();	
+	}
+}
---
+++ Snake Game/src/SnakeCanvas.java
@@ -0,0 +1,22 @@
+import java.awt.Color;
+import java.io.IOException;
+
+import javax.sound.sampled.LineUnavailableException;
+import javax.sound.sampled.UnsupportedAudioFileException;
+import javax.swing.ImageIcon;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+
+public class SnakeCanvas extends JFrame{
+		
+	SnakeCanvas() throws UnsupportedAudioFileException, IOException, LineUnavailableException{
+		
+		this.add(new SnakePanel());
+		this.setTitle("Snake");
+		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		this.setResizable(false);
+		this.pack();
+		this.setVisible(true);
+		this.setLocationRelativeTo(null);
+	}
+}
